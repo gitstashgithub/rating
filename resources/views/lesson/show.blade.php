5cc056d3759e20dac9abe33a429c7515eedbba34
vagrant <vagrant@homestead>
2016-02-09 12:50:57 +0000

    fix typo

--- resources/views/lesson/show.blade.php
+++ resources/views/lesson/show.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="panel panel-default">
-                    <div class="panel-heading">{{$lesson->Description}}</div>
+                    <div class="panel-heading">{{$lesson->description}}</div>
                     <div class="panel-body">
                         <form class="form-horizontal" role="form" method="POST" action="{{ url('/rating') }}">
                             {!! csrf_field() !!}
